2420e635bc051fa93056e2b7751083d94a66504e
AlexandraGradinaru <72404535+AlexandraGradinaru@users.noreply.github.com>
2021-07-11 16:51:16 +0300

    Update arboriClasificare.r

--- arboriClasificare.r
+++ arboriClasificare.r
@@ -6,10 +6,9 @@ library(rsample)
 library(caret)
 library(partykit)
 
-wine <- read_csv2("wine.csv") #incarcarea setulu de date
+wine <- read_csv2("wine.csv") #upload dataset
 
 wine %>% ggplot(aes(alcohol)) + geom_density()
-
 wine_clas <- wine %>% 
   mutate(alcohol = ifelse(alcohol <11, "No", "Yes"))
 wine_clas <- wine_clas %>% 
@@ -22,28 +21,21 @@ wine_clas_train <- training(wine_clas_split)
 wine_clas_test <- testing(wine_clas_split)
 
 table(wine_clas_train$alcohol)
-table(wine_clas_test$alcohol) # 35% din vinuri alcoolemie mai mare de 10%
-
-set.seed(123)
+table(wine_clas_test$alcohol) # 35% of wines have alchohol percentage > 10%
 
 c1 = rpart(
   formula = alcohol ~ .,
   data = wine_clas_train,
   method = "class"
 )
-
 c1
 summary(c1)
 rpart.plot(c1)
-
 pred_c1 <- predict(c1, newdata = wine_clas_test, target = "class" )
 pred_c1 <- as_tibble(pred_c1)%>% mutate(class = ifelse(No >= Yes, "No", "Yes"))
 table(pred_c1$class, wine_clas_test$alcohol)
+confusionMatrix(factor(pred_c1$class), factor(wine_clas_test$alcohol)) # 90% accuracy
 
-confusionMatrix(factor(pred_c1$class), factor(wine_clas_test$alcohol)) # acuratete de 90%
-
-
-set.seed(123)
 c1_pruned <- prune(c1, cp=0.01)
 c1_pruned
 
@@ -55,39 +47,30 @@ c2 <- rpart( alcohol ~ .,
 c2
 summary(c2)
 rpart.plot(c2)
-
-
 pred_c2 <- predict(c2, newdata = wine_clas_test, target = "class" )
 pred_c2 <- as_tibble(pred_c2)%>% mutate(class = ifelse(No >= Yes, "No", "Yes"))
 confusionMatrix(factor(pred_c2$class), factor(wine_clas_test$alcohol))
 
 c2_pruned <- prune(c2, cp=0.02)
 summary(c2_pruned)
-
 pred_c2_pruned <- predict(c2_pruned, newdata = wine_clas_test, target = "class" )
 pred_c2_pruned <- as_tibble(pred_c2_pruned)%>% mutate(class = ifelse(No >= Yes, "No", "Yes"))
 confusionMatrix(factor(pred_c2_pruned$class), factor(wine_clas_test$alcohol))
 
 
 library(tree)
-
-set.seed(123)
 c1_tree <- tree(alcohol ~ .,
                 data = wine_clas_train)
 c1_tree
 summary(c1_tree)
-
 pred_c1_tree <- predict(c1_tree, newdata = wine_clas_test, target = "class" )
 pred_c1_tree <- as_tibble(pred_c1_tree)%>% mutate(class = ifelse(No >= Yes, "No", "Yes"))
 confusionMatrix(factor(pred_c1_tree$class), factor(wine_clas_test$alcohol))
 
-
-set.seed(123)
 c1_tree_gini <- tree(alcohol ~ .,
                 data = wine_clas_train)
 c1_tree_gini
 summary(c1_tree_gini)
-
 pred_c1_tree_gini <- predict(c1_tree_gini, newdata = wine_clas_test, target = "class" )
 pred_c1_tree_gini <- as_tibble(pred_c1_tree_gini)%>% mutate(class = ifelse(No >= Yes, "No", "Yes"))
 confusionMatrix(factor(pred_c1_tree_gini$class), factor(wine_clas_test$alcohol))
